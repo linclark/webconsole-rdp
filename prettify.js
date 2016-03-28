8bfe2e486646636d0356499b1c7f76c0abd91723
Lin Clark <lclark@mozilla.com>
2016-03-28 16:22:51 -0400

    handle HTML prettification

--- prettify.js
+++ prettify.js
@@ -1,12 +1,22 @@
+const fs = require('fs');
 const finder = require('findit')(process.argv[2] || './data');
 const path = require('path');
 const jsonfile = require('jsonfile');
+const pretty = require('pretty');
 
 finder.on('file', function (file, stat) {
-  const obj = jsonfile.readFileSync(file)
-  jsonfile.writeFile(file, obj, {spaces: 2}, function (err) {
-    if (err) {
-      console.error(err)
-    }
-  })
+
+  if (file.includes(".json")) {
+    const obj = jsonfile.readFileSync(file)
+    jsonfile.writeFile(file, obj, {spaces: 2}, function (err) {
+      if (err) {
+        console.error(err)
+      }
+    })
+  }
+  else {
+    const string = fs.readFileSync(file, "utf8")
+    fs.writeFileSync(file, pretty(string));
+  }
+
 });
